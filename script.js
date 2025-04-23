17a4a75a128f3e42da6ac4e0f8438f673fa0c388
Hagi <Hagiazzamaz@gmail.com>
2025-04-23 07:04:48 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function penjumlahan(bil1, bil2) {
+  // memberikan hasil penjumlahan
+  return bil1 + bil2;
+}
+// menjalankan penjumlahan 12 + 10
+const hasil = penjumlahan(12, 10);
+console.log(hasil); // output: 22
